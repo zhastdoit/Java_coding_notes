6b151a52300beb685c783b3c0db71d971c49bbb5
Xiaojie Zha <xz1776@nyu.edu>
2017-09-27 18:09:34 -0400

    Create Coding habits.java

---
+++ Coding habits.java
@@ -0,0 +1,12 @@
+
+
+//Main Function
+public class fall {
+  public static void main(String[] args){
+    if((args==null) || (args.length!=1)){
+      throw new IllegalArgumentException("Argument are required");
+    }
+    if("Foo".quals(args[0]){
+      //TODO
+    }
+  }
